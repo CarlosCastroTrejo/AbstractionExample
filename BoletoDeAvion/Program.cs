fcb9f5365f2f525c70ab912b608a912100c45435
Carlos Emiliano Castro Trejo <carlos_castro5156@outlook.es>
2018-08-12 12:14:49 -0500

    The abstract program of the Airplane ticket is finished. It's methods were completed coded aand the main() represents a complete full ticket and a error ticket, both work wwell.

--- BoletoDeAvion/Program.cs
+++ BoletoDeAvion/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Estructura de Datos 
+// Jorge Romero Romanis A01423263
+// Carlos Castro Trejo A01422062
+using System;
 
 namespace BoletoDeAvion
 {
@@ -7,11 +10,24 @@ namespace BoletoDeAvion
         public static void Main(string[] args)
         {
             Boletos uno = new Boletos();
-            Console.WriteLine(uno.codigoBarras(123, "Carlos", "AEROMEXICO"));
+            Console.WriteLine(uno.llenarDatos("Carlos", 123, "AEORMEXICO", "MEX", "MIA", "B303", "Desayuno","EliteBlack"));
+            Console.WriteLine(uno.codigoBarras());
+            Console.WriteLine(uno.baseDeDatos(123));
+            Console.WriteLine(uno.Imprimir(uno.codigoBarras()));
+
+            Console.WriteLine("--------------------");
+
+            Boletos dos = new Boletos();
+            Console.WriteLine(dos.llenarDatos("Jorge#", 563, "INTERJET", "MEX", "LAX", "C506", "Cena", "Turista"));
+            Console.WriteLine(dos.codigoBarras());
+            Console.WriteLine(dos.baseDeDatos(563));
+            Console.WriteLine(dos.Imprimir(dos.codigoBarras()));
+            Console.ReadKey();
         }
     }
     public class Boletos
     {
+
         //Atributos
         /*
             Fecha/Hora Partida: Fecha y hora de salida del origen
@@ -33,9 +49,6 @@ namespace BoletoDeAvion
         string sDestino;
         //Destino: Nombre del aeropuerto de donde va a llegar
 
-        int iCodigo;
-        //Código de Barras: Código único a escanear que representa los datos del boleto
-
         string sAvion;
         //Tipo de Avión: Avión que se va a abordar
 
@@ -45,56 +58,152 @@ namespace BoletoDeAvion
         string sClase;
         //Clase: Clase de servicio del vuelo
 
+        int aleatorio = 0;
 
         //Constructor
         public Boletos()
         {
-            
-        }
 
-        public bool llenarDatos(string sNOmbre,int iVuelo,string sAerolinea, string sOrigen,string sDestino,int iCodigo, string sAvion, string sServicios,string sClase)
-        {
-            this.sNOmbre = sNOmbre;
-            this.iVuelo = iVuelo;
-            this.sAerolinea = sAerolinea;
-            this.sOrigen = sOrigen;
-            this.sDestino = sDestino;
-            this.iCodigo = iCodigo;
-            this.sAvion = sAvion;
-            this.sServicios = sServicios;
-            this.sClase = sClase;
-            return true;
         }
+        //Metodos
         /*
-        Llenar Datos: Cargar los datos a la base de datos de la aerolínea
-        Input: Persona escriba los datos en la correspondiente casilla
-        Output: Mensaje de carga exitosa y de envío de datos a la base de datos
-        */
-
-        public string codigoBarras(int iVuelo,string sNombre,string sAerolinea)
+            Llenar Datos: Cargar los datos a la base de datos de la aerolínea
+            Input: Persona escriba los datos en la correspondiente casilla
+            Output: Mensaje de carga exitosa y de envío de datos a la base de datos
+         */
+        public string llenarDatos(string sNOmbre, int iVuelo, string sAerolinea, string sOrigen, string sDestino, string sAvion, string sServicios, string sClase)
         {
+            if(comprobacion(sNOmbre))
+            {
+                this.sNOmbre = sNOmbre;
+            }else
+            {
+                return "Error en el Nombre";
+            }
+            this.iVuelo = iVuelo;
+            if (comprobacion(sAerolinea)) 
+            {
+                this.sAerolinea = sAerolinea;            
+            }
+            else
+            {
+                return "Error en la Aerolinea";
+            }
+
+            if (comprobacion(sOrigen))
+            {
+                this.sOrigen = sOrigen;
+            }
+            else
+            {
+                return "Error en el Origen";
+            }
+
+            if (comprobacion(sDestino))
+            {
+                this.sDestino = sDestino;
+            }
+            else
+            {
+                return "Error en el Destino";
+            }
+
+            if (comprobacion(sAvion))
+            {
+                this.sAvion = sAvion;
+            }
+            else
+            {
+                return "Error en el Avion";
+            }
+            if (comprobacion(sServicios))
+            {
+                this.sServicios = sServicios;
+            }
+            else
+            {
+                return "Error en los Servicios";
+            }
+
+            if (comprobacion(sClase))
+            {
+                this.sClase = sClase;
+            }
+            else
+            {
+                return "Error en la Clase";
+            }
+
+
             Random random = new Random();
-            int aleatorio=random.Next(100000, 90000000);
-            return aleatorio+iVuelo+sNombre;
+            this.aleatorio = random.Next(100000, 90000000);
+
+            return "Llenados de datos con exito";
+        }
+
+        /*
+            Generar Código de Barras: 
+            Input: numero de vuelo, nombre de cliente y aerolínea
+            Output: Código unico para abordar
+        */
+        public string codigoBarras()
+        {
+           
+            return this.aleatorio + this.iVuelo + this.sNOmbre + this.sAerolinea;
+        }
+
+        /*
+            Conectarse a Base de Datos de Aerolínea:  Checar y verificar datos del vuelo
+            Input: Número de vuelo
+            Output: Horarios del vuelo
+         */
+        public string baseDeDatos(int iVuelo)
+        {
+            string[] horarios = new string[4] { "10:00PM", "2:50PM", "11:30AM", "6:59AM" };
+            int[] vuelos = new int[4] { 123,563,189,4563 };
+
+            for (int x = 0; x < vuelos.Length;x++)
+            {
+                if(iVuelo==vuelos[x])
+                {
+                    return "Tu vuelo sale a la hora: " + horarios[x];
+                    break;
+                }
+            }
+            return "Tu vuelo todavia no ha sido asignado";
+        }
+
+        /*
+            Imprimirse:  Imprimir el check-in
+            Input: Código unico para abordar
+            Output: Mandar formulario a la impresora y aviso de exito
+         */
+        public string Imprimir(string codigoDeAbordar)
+        {
+
+            return "Se ha impreso el formulario del codigo: "+codigoDeAbordar;
+        }
+
+        /*
+            Comprobación de datos: Comprobar que el dato vaya acorde al tipo de dato del formulario
+            Input: Dato del formulario
+            Output: Mensaje de datos validos
+        */
+        public bool comprobacion(string dato)
+        {
+            char[] arreglo = new char[14] { '@', '#', '$', '%', '^', '&', '*', '(', ')', '=', '+', '-', '_', '~' };
+            for (int x = 0; x < arreglo.Length; x++)
+            {
+                if (dato.Contains(arreglo[x].ToString()))
+                {
+                    return false;
+                    break;
+                }
+            }
+
+            return true;
         }
 
     }
+
 }
-/*
-
-Generar Código de Barras: 
-        Input: numero de vuelo, nombre de cliente y aerolínea
-        Output: Código unico para abordar
-Conectarse a Base de Datos de Aerolínea:  Checar y verificar datos del vuelo
-
-        Input: Número de vuelo
-        Output: Horarios del vuelo
-Imprimirse:  Imprimir el check-in
-        Input: Código unico para abordar
-
-        Output: Mandar formulario a la impresora
-Comprobación de datos: Comprobar que el dato vaya acorde al tipo de dato del formulario
-
-        Input: Dato del formulario
-        Output: Mensaje de datos validos
-*/
